6af4c616a515c7c28413593929c8ed089724f9b9
kjxlau <59328547+kjxlau@users.noreply.github.com>
2024-02-24 23:26:14 +0800

    Update DownloadFasta.py

--- DownloadFasta.py
+++ DownloadFasta.py
@@ -3,9 +3,10 @@ from Bio import Entrez
 Entrez.email = "kennyjxlau@gmail.com"
 Entrez.api_key = "6b675e217b0d870e6ca896cd257b4c073708"
 
-IDs = Entrez.read(Entrez.esearch(db="nucleotide", retmax=1000, term="DENV [Organism]")) ["IdList"]
-with open('Xylaria.fasta', 'w') as f:
+IDs = Entrez.read(Entrez.esearch(db="nucleotide", retmax=10, term="Dengue Singapore")) ["IdList"]
+with open('DENV_SG.fasta', 'w') as f:
     for ID in IDs:
         seq=Entrez.efetch(db="nucleotide", id=ID, rettype="fasta", retmode="text").read()
         print(seq)
         f.write(seq)
+
