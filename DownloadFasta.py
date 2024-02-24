9bbd06d61d26cc035eff67e530f3ddf8c6c88b43
kjxlau <59328547+kjxlau@users.noreply.github.com>
2024-02-24 23:59:45 +0800

    Update DownloadFasta.py

--- DownloadFasta.py
+++ DownloadFasta.py
@@ -5,13 +5,16 @@ Entrez.api_key = "6b675e217b0d870e6ca896cd257b4c073708"
 
 no_of_records=input("Enter No of Records to download: ")
 organism=input("Enter organism of interest: ")
-#DENV1: txid11053[Organism:exp]
-#DENV2: txid11060[Organism:exp]
-#DENV3: txid11069[Organism:exp]
-#DENV4: txid11070[Organism:exp]
-gene=input("Enter target gene: ")
-country=input("Country of interest: ")
-search_term=organism+str(" [organism] AND ")+gene+str(" [gene] AND ")+country+str(" [All Fields]")
+#DENV1: txid11053
+#DENV2: txid11060
+#DENV3: txid11069
+#DENV4: txid11070
+country1=input("Country 1 of interest: ")
+country2=input("Country 2 of interest: ")
+country3=input("Country 3 of interest: ")
+#Singapore AND Thailand 
+search_term=organism+str("[organism] AND ")+country1+str("[All Fields] OR ")+country2+str("[All Fields] OR ")+country3+str("[All Fields]")
+print(search_term)
 
 IDs = Entrez.read(Entrez.esearch(db="nucleotide", retmax=no_of_records, term=search_term)) ["IdList"]
 filename=input("Filename to save as fasta: ")+str(".fasta")
