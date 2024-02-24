61ddc7452b4fc4ee0ec43922cbeae2a576ca8aff
kjxlau <59328547+kjxlau@users.noreply.github.com>
2024-02-24 23:48:17 +0800

    Update DownloadFasta.py

--- DownloadFasta.py
+++ DownloadFasta.py
@@ -3,12 +3,19 @@ from Bio import Entrez
 Entrez.email = "kennyjxlau@gmail.com"
 Entrez.api_key = "6b675e217b0d870e6ca896cd257b4c073708"
 
-IDs = Entrez.read(Entrez.esearch(db="nucleotide", retmax=10, term="txid11053[Organism:exp] AND Thailand[All Fields]")) ["IdList"]
+no_of_records=input("Enter No of Records to download: ")
+organism=input("Enter organism of interest: ")
 #DENV1: txid11053[Organism:exp]
 #DENV2: txid11060[Organism:exp]
 #DENV3: txid11069[Organism:exp]
 #DENV4: txid11070[Organism:exp]
-with open('DENV1_Thailand.fasta', 'w') as f:
+gene=input("Enter target gene: ")
+country=input("Country of interest: ")
+search_term=organism+str(" [organism] AND ")+gene+str(" [gene] AND ")+country+str(" [All Fields]")
+
+IDs = Entrez.read(Entrez.esearch(db="nucleotide", retmax=no_of_records, term=search_term)) ["IdList"]
+filename=input("Filename to save as fasta: ")+str(".fasta")
+with open(filename, 'w') as f:
     for ID in IDs:
         seq=Entrez.efetch(db="nucleotide", id=ID, rettype="fasta", retmode="text").read()
         print(seq)
