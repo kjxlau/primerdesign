9ef8e3b74dceac5a8a70a91e4e1fc8123c642102
kjxlau <59328547+kjxlau@users.noreply.github.com>
2024-03-05 11:46:09 +0800

    Update Step3_LampPrimers.py

--- Step3_LampPrimers.py
+++ Step3_LampPrimers.py
@@ -46,6 +46,7 @@ for n1 in listprimern:
             count=0;
 F2=[];F2Loc=[];F2Len=[]
 B2=[];B2Loc=[];B2Len=[]
+count=0
 #Find F2 and B2 primers
 listprimern2=[]
 for n in df2.index:
@@ -54,29 +55,38 @@ for n1 in listprimern2:
     for n2 in listprimern2:
         primerdf2=abs(df2["F3Loc"][n1]-df2["B3Loc"][n2])
         if primerdf2<=160 and int(df2["F3Loc"][n2]+df2["F3Len"][n2])>int(df2["F3Loc"][n1]):
-            F2.append(df2["F3"][n1])
-            F2Loc.append(df2["F3Loc"][n1])
-            F2Len.append(df2["F3Len"][n1])
-            B2.append(df2["B3"][n2])
-            B2Loc.append(df2["B3Loc"][n2])
-            B2Len.append(df2["B3Len"][n2])
-            df3=pd.DataFrame(list(zip(F2,F2Loc,F2Len,B2,B2Loc,B2Len)),columns=['F2','F2Loc','F2Len','B2','B2Loc','B2Len'])
-            df3.to_csv("df3"+".csv",index=False)
+            count+=1
+            if count<=3:
+                F2.append(df2["F3"][n1])
+                F2Loc.append(df2["F3Loc"][n1])
+                F2Len.append(df2["F3Len"][n1])
+                B2.append(df2["B3"][n2])
+                B2Loc.append(df2["B3Loc"][n2])
+                B2Len.append(df2["B3Len"][n2])
+                df3=pd.DataFrame(list(zip(F2,F2Loc,F2Len,B2,B2Loc,B2Len)),columns=['F2','F2Loc','F2Len','B2','B2Loc','B2Len'])
+                df3.to_csv("df3"+".csv",index=False)
+        else:
+            count=0;
             
 F1c=[];F1cLoc=[];F1cLen=[]
 B1c=[];B1cLoc=[];B1cLen=[]
-#Find F2 and B2 primers
+count=0
+#Find F1c and B1c primers
 listprimern3=[]
 for n in df3.index:
 	listprimern3.append(n)
 for n1 in listprimern3:
     for n2 in listprimern3:
         if int(df3["F2Loc"][n2]+df3["F2Len"][n2])>int(df3["F2Loc"][n1]) and (int(df3["B2Loc"][n2])<int(df3["B2Loc"][n1])) :
-            F1c.append((df3["F2"][n1]).reverse_complement())
-            F1cLoc.append(df3["F2Loc"][n1])
-            F1cLen.append(df3["F2Len"][n1])
-            B1c.append((df3["B2"][n2]).reverse_complement())
-            B1cLoc.append(df2["B2Loc"][n2])
-            B1cLen.append(df2["B2Len"][n2])
-            df4=pd.DataFrame(list(zip(F1c,F1cLoc,F1cLen,B1c,B1cLoc,B1cLen)),columns=['F1c','F1cLoc','F1cLen','B1c','B1cLoc','B1cLen'])
-            df4.to_csv("df4"+".csv",index=False)
+            count+=1
+            if count<=3:
+                F1c.append((df3["F2"][n1]).reverse_complement())
+                F1cLoc.append(df3["F2Loc"][n1])
+                F1cLen.append(df3["F2Len"][n1])
+                B1c.append((df3["B2"][n2]).reverse_complement())
+                B1cLoc.append(df2["B2Loc"][n2])
+                B1cLen.append(df2["B2Len"][n2])
+                df4=pd.DataFrame(list(zip(F1c,F1cLoc,F1cLen,B1c,B1cLoc,B1cLen)),columns=['F1c','F1cLoc','F1cLen','B1c','B1cLoc','B1cLen'])
+                df4.to_csv("df4"+".csv",index=False)
+        else:
+            count=0;
