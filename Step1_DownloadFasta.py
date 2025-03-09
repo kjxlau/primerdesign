6bc64d33590ce4591d54f0ef155abad7070b354a
Kenny JX Lau <59328547+kjxlau@users.noreply.github.com>
2025-03-09 21:25:25 +0800

    Update Step1_DownloadFasta.py

--- Step1_DownloadFasta.py
+++ Step1_DownloadFasta.py
@@ -6,17 +6,13 @@ Entrez.api_key = "6b675e217b0d870e6ca896cd257b4c073708"
 no_of_records=input("Enter No of Records to download: ")
 organism=input("Enter organism of interest: ")
 
-country1=input("Country 1 of interest: ")
-country2=input("Country 2 of interest: ")
-country3=input("Country 3 of interest: ")
-
-search_term=organism+str("[organism:exp] AND (")+country1+str("[All Fields] OR ")+country2+str("[All Fields] OR ")+country3+str("[All Fields])")
+search_term=organism
 print(search_term)
 
-IDs = Entrez.read(Entrez.esearch(db="nucleotide", retmax=no_of_records, term=search_term)) ["IdList"]
+IDs = Entrez.read(Entrez.esearch(db="protein", retmax=no_of_records, term=search_term)) ["IdList"]
 filename=input("Filename to save as fasta: ")+str(".fasta")
 with open(filename, 'w') as f:
     for ID in IDs:
-        seq=Entrez.efetch(db="nucleotide", id=ID, rettype="fasta", retmode="text").read()
+        seq=Entrez.efetch(db="protein", id=ID, rettype="fasta", retmode="text").read()
         print(seq)
         f.write(seq)
