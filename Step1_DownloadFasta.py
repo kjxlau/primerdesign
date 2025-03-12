cc04b4ac1a74a9385bb737716b319454a802661f
Kenny JX Lau <59328547+kjxlau@users.noreply.github.com>
2025-03-12 15:41:43 +0800

    Update Step1_DownloadFasta.py

--- Step1_DownloadFasta.py
+++ Step1_DownloadFasta.py
@@ -9,7 +9,7 @@ organism=input("Enter organism of interest: ")
 search_term=organism
 print(search_term)
 
-IDs = Entrez.read(Entrez.esearch(db="protein", retmax=no_of_records, term=search_term)) ["IdList"]
+IDs = Entrez.read(Entrez.esearch(db="nucleotide", retmax=no_of_records, term=search_term)) ["IdList"]
 filename=input("Filename to save as fasta: ")+str(".fasta")
 with open(filename, 'w') as f:
     for ID in IDs:
